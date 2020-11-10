23c4a0595fd158bf07baf609e4f5cb6f44f2b3ee
Grégory Planchat <gregory@kiboko.fr>
2020-11-11 00:34:33 +0100

    Fixing private key deciphering

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh -l
 
 echo "$BOX_SECRET_KEY" > kloud-private.pem
-echo "$BOX_SECRET_KEY_PASSPHRASE" > kloud-private.passphrase
 
-openssl rsa -in kloud-private.pem --passin kloud-private.passphrase -out kloud-private-nopassphrase.pem
+openssl rsa -in kloud-private.pem -passin pass:"$BOX_SECRET_KEY_PASSPHRASE" -out kloud-private-nopassphrase.pem
 
 box build
-
-echo ::set-output name=phar::kloud.phar
-echo ::set-output name=pubkey::kloud.phar.pubkey
